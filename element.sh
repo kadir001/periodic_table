ece82c9fa2bb5f65d2a90ce68130085830b4087c
Kadir Akdemir <kadirakdemir75@gmail.com>
2024-12-21 11:23:30 +0000

    feat: add functional script for querying periodic table elements

--- element.sh
+++ element.sh
@@ -1,31 +1,30 @@
 #!/bin/bash
 
-# Database query command
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
-# Check for argument
-if [[ -z $1 ]]
-then
+# Check if an argument is provided
+if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
   exit
 fi
 
-# Query the database
-ELEMENT=$($PSQL "SELECT elements.atomic_number, elements.name, elements.symbol, types.type, properties.atomic_mass, properties.melting_point_celsius, properties.boiling_point_celsius 
-                 FROM elements 
-                 INNER JOIN properties ON elements.atomic_number = properties.atomic_number
-                 INNER JOIN types ON properties.type_id = types.type_id 
-                 WHERE elements.atomic_number = '$1' OR elements.symbol = '$1' OR elements.name ILIKE '$1'")
+# Determine input type and query database
+if [[ $1 =~ ^[0-9]+$ ]]; then
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE atomic_number=$1")
+elif [[ $1 =~ ^[A-Z][a-z]?$ ]]; then
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE symbol='$1'")
+else
+  ELEMENT=$($PSQL "SELECT atomic_number, name, symbol, type, atomic_mass, melting_point_celsius, boiling_point_celsius FROM elements INNER JOIN properties USING(atomic_number) INNER JOIN types USING(type_id) WHERE name='$1'")
+fi
 
-# Handle no results
-if [[ -z $ELEMENT ]]
-then
+# Check if element was found
+if [[ -z $ELEMENT ]]; then
   echo "I could not find that element in the database."
   exit
 fi
 
-# Parse the results
-IFS="|" read -r ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT"
+# Parse the query result
+IFS="|" read ATOMIC_NUMBER NAME SYMBOL TYPE MASS MELTING BOILING <<< "$ELEMENT"
 
-# Output the results
+# Output the element details
 echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELTING celsius and a boiling point of $BOILING celsius."
